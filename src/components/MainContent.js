d1db8f5104ffc3e009c07a5a1d739d6630853361
cleverRay <Raymondehimwenma@gmail.com>
2022-12-05 11:21:26 +0100

    firstcommit

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -136,7 +136,7 @@ function MainContent() {
                                 </Col>
                                 <Col sm={10}>
                                 <p>Varelas</p>
-                                <p>@varel</p>
+                                <p>@varels</p>
                                 </Col>
                             </Row>
                         </div>
