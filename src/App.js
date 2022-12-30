e4323d0372a27343831037d75ea4f7248439464c
cleverRay <Raymondehimwenma@gmail.com>
2022-12-30 06:10:10 +0100

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -68,9 +68,9 @@ function App() {
     <div>
       
       <Routes>
-      <Route path='/' element={ <Homepage PricingStorage={cardPricing} handleCategory={handleCategory} />} />
-      <Route path='/RegisterPage' element={ <RegisterPage/>} />
-      <Route path='/PriceList' element={ <PriceList PricingStorage={cardPricing} handleCategory={handleCategory} />} />
+      <Route path='/Squadbyte' element={ <Homepage PricingStorage={cardPricing} handleCategory={handleCategory} />} />
+      <Route path='/Squadbyte/RegisterPage' element={ <RegisterPage/>} />
+      <Route path='/Squadbyte/PriceList' element={ <PriceList PricingStorage={cardPricing} handleCategory={handleCategory} />} />
       <Route path='*' element={<NotFoundPage/>}/>
       
       </Routes>
